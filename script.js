c9f9218a12a64749ca5e1e44914d677917fe64ad
Cheeeeez <nguyentrongnama8@gmail.com>
2020-03-26 11:44:03 +0700

    add validate

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let Student = function (fullName, gender, dateOfBirth, email, phone, homeTown) {
 
 let StudentList = function () {
     this.list = [];
-    this.currentIndex = DEFAULT_INDEX;      // Để xác định phương thức thêm hoặc sửa
+    this.currentIndex = DEFAULT_INDEX;
 
     this.updateList = function () {
         let fullName = getElementValue('full-name');
@@ -36,15 +36,24 @@ let StudentList = function () {
         let phone = getElementValue('phone');
         let homeTown = getElementValue('hometown');
 
-        if (this.currentIndex === DEFAULT_INDEX) {
-            let student = new Student(fullName, gender, dateOfBirth, email, phone, homeTown);
-            this.list.push(student);
-            this.showList();
+        if (!validateEmpty('full-name', fullName) && !validateEmpty('date-of-birth', dateOfBirth) &&
+            validateEmail('email', email) && !validateEmpty('phone', phone) && validatePhone('phone', phone)) {
+            if (this.currentIndex === DEFAULT_INDEX) {
+                let student = new Student(fullName, gender, dateOfBirth, email, phone, homeTown);
+                this.list.push(student);
+                this.setStorage();
+                this.showList();
+                console.log(getElementValue('date-of-birth').length);
+            } else {
+                this.list[this.currentIndex] = new Student(fullName, gender, dateOfBirth, email, phone, homeTown);
+                this.setStorage();
+                this.showList();
+                this.currentIndex = DEFAULT_INDEX;
+                getElementID('save').value = "Thêm mới";
+            }
         } else
-            this.list[this.currentIndex] = new Student(fullName, gender, dateOfBirth, email, phone, homeTown);
-        this.showList();
-        this.currentIndex = DEFAULT_INDEX;
-        getElementID('save').value = "Thêm mới";
+            return false;
+
     };
 
     this.showList = function () {
@@ -67,7 +76,7 @@ let StudentList = function () {
         this.currentIndex = index;
         let student = this.list[index];
         getElementID('full-name').value = student.fullName;
-        if (student.gender === getElementID('male').value) {
+        if (student.gender === getElementValue('male')) {
             getElementID('male').checked;
         } else
             getElementID('female').checked;
@@ -76,20 +85,98 @@ let StudentList = function () {
         getElementID('phone').value = student.phone;
         getElementID('hometown').value = student.homeTown;
         getElementID('save').value = "Sửa";
-        console.log(student);
     };
 
     this.deleteStudent = function (index) {
         this.list.splice(index, 1);
+        this.setStorage();
         this.showList();
+    };
+
+    this.setStorage = function () {
+        let jsonList = JSON.stringify(this.list);
+        localStorage.setItem('Students', jsonList)
+    };
+
+    this.getStorage = function () {
+        if (localStorage.length > 0) {
+            let jsonList = localStorage.getItem('Students');
+            this.list = JSON.parse(jsonList);
+            this.showList();
+        }
+    };
+
+    this.searchStudent = function (keyword) {
+        let searchList = new StudentList();
+        for (let i = 0; i < this.list.length; i++) {
+            if (deleteAccents(this.list[i].fullName).toLowerCase().trim().search(deleteAccents(keyword).toLowerCase().trim()) !== -1)
+                searchList.list.push(this.list[i]);
+            searchList.showList();
+        }
+
     }
 };
 
 let studentList = new StudentList();
 
-function main() {
-    studentList.updateList();
+function deleteAccents(str) {
+    str = str.replace(/[àáạảãâầấậẩẫăằắặẳẵ]/g, "a");
+    str = str.replace(/[èéẹẻẽêềếệểễ]/g, "e");
+    str = str.replace(/[ìíịỉĩ]/g, "i");
+    str = str.replace(/[òóọỏõôồốộổỗơờớợởỡ]/g, "o");
+    str = str.replace(/[ùúụủũưừứựửữ]/g, "u");
+    str = str.replace(/[ỳýỵỷỹ]/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/[ÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴ]/g, "A");
+    str = str.replace(/[ÈÉẸẺẼÊỀẾỆỂỄ]/g, "E");
+    str = str.replace(/[ÌÍỊỈĨ]/g, "I");
+    str = str.replace(/[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]/g, "O");
+    str = str.replace(/[ÙÚỤỦŨƯỪỨỰỬỮ]/g, "U");
+    str = str.replace(/[ỲÝỴỶỸ]/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    return str;
 }
 
+function isEmptyInput(value) {
+    if (value.trim() === "") {
+        return true;
+    }
+}
 
+function isNotANumber(value) {
+    if (Number.isNaN(value))
+        return true;
+}
 
+function validateEmpty(id, value) {
+    if (isEmptyInput(value)) {
+        getElementID(id).style.borderColor = "red";
+        return true;
+    } else {
+        getElementID(id).style.borderColor = "green";
+        return false;
+    }
+}
+
+function validatePhone(id, value) {
+    if (!isNotANumber(value) && value.length >= 10) {
+        getElementID(id).style.borderColor = "green";
+        return true;
+    } else {
+        getElementID(id).style.borderColor = "red";
+        return false;
+    }
+}
+
+function validateEmail(id, value) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(value).trim())) {
+        getElementID(id).style.borderColor = "green";
+        return true;
+    } else {
+        getElementID(id).style.borderColor = "red";
+        return false;
+    }
+}
+
+console.log(getElementValue('date-of-birth'));
